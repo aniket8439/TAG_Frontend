f0c9ab18981feb9ce0600e6bba6da68111bcf2e1
Yash-death <yashbadoniya1@gmail.com>
2023-11-04 00:36:34 +0530

    Signin & SignUp page Done

---
+++ src/components/SignUp.js
@@ -0,0 +1,72 @@
+import React from "react";
+function SignUpForm() {
+  const [state, setState] = React.useState({
+    name: "",
+    phone_number:"",
+    email: "",
+    password: ""
+  });
+  const handleChange = evt => {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value
+    });
+  };
+
+  const handleOnSubmit = evt => {
+    evt.preventDefault();
+
+    const { name,phone_number,email, password } = state;
+    alert(
+      `You are sign up with name: ${name} phone number: ${phone_number} email: ${email} and password: ${password}`
+    );
+
+    for (const key in state) {
+      setState({
+        ...state,
+        [key]: ""
+      });
+    }
+  };
+
+  return (
+    <div className="form-container sign-up-container">
+      <form onSubmit={handleOnSubmit}>
+        <h1>Create Account</h1>
+        <br/>
+        <input
+          type="text"
+          name="name"
+          value={state.name}
+          onChange={handleChange}
+          placeholder="Name"
+        />
+        <input
+          type="number"
+          placeholder="Phone number"
+          name="phone number"
+          value={state.phone_number}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          value={state.password}
+          onChange={handleChange}
+          placeholder="Password"
+        />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUpForm;
